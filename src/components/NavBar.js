f6c385376f3a8b080a7ab9d7b59b3687861886eb
Luis Gonzalez <lugo50635@gmail.com>
2021-07-27 19:16:42 -0300

    segundo desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <>
+            <nav className="nav col-4 align-self-center">
+                <a className="nav-link active" aria-current="page" href="#">Shop</a>
+                <a className="nav-link disabled" href="#">Learn</a>
+                <a className="nav-link disabled" href="#">Visit us</a>
+            </nav>
+
+            <img src="img/fnwLogo.png" className="col-4"/>
+
+            <div className="col-4 text-end align-self-center">
+                <a href="#" className="text-decoration-none">Cart</a>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
